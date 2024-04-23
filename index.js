b4c8125b9936cc83799be5e99059a2a70ca8b987
ThandoK03 <160265548+ThandoK03@users.noreply.github.com>
2024-04-23 10:42:26 +0200

    Added a fuction to the js

---
+++ index.js
@@ -0,0 +1,9 @@
+function getWeather() {
+  let apiKey = "853f9fa9a83fbe77003t40d42014oc36";
+
+  let city = document.getElementById("city").value;
+  if (!city) {
+    alert("Please enter a city");
+    return;
+  }
+}
