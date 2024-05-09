8c646b460d91561c0813388464cadd3615a7fac1
ThandoK03 <160265548+ThandoK03@users.noreply.github.com>
2024-05-09 07:39:37 +0200

    updated the form section

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function displayTemperature(response) {
-  let temperatureElement = document.querySelector("temp");
-  let temperature = math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temp");
+  let temp = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
-
+  console.log(response);
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = response.data.temperature.current;
 }
 
 function searchCity(city) {
   let apiKey = "853f9fa9a83fbe77003t40d42014oc36";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayTemperature);
 }
 
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", search);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Lisbon");
+searchCity("Paris");
