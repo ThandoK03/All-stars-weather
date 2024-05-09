a4448984884eac9dd3785140e6531da22a28d2d6
ThandoK03 <160265548+ThandoK03@users.noreply.github.com>
2024-05-09 06:59:31 +0200

    added search functionality using api

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function updateWeatherData(response) {}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("temp");
+  let temperature = math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
 
 function searchCity(city) {
   let apiKey = "853f9fa9a83fbe77003t40d42014oc36";
-  let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
-  axios.get(apiUrl).then(updateWeatherData);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}}`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function searchSubmit(event) {
+function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchSubmit);
+searchFormElement.addEventListener("submit", search);
+
+searchCity("Lisbon");
